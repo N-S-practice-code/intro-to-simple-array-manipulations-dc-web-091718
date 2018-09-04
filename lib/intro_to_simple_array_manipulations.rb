a77a2e36d616f5ff8e868e63d835db9a0d4da708
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-04 20:00:28 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,12 @@
 def using_push(array,el)
   array.push(el)
 end
+def using_unshift(array,el)
+  array.unshift(el)
+en
+def using_pop(array,el)
+  array.pop(el)
+end
+def using_push(array,el)
+  array.push(el)
+end
