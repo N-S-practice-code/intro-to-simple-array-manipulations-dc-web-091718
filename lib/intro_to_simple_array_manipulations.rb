598e509ed580d1cb01c0a1c518b88584b2713e3e
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-04 20:10:43 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,7 +17,7 @@ def shift_with_args(array)
   array.shift(2)
 end
 def using_concat(array1,array2)
-  array.concat(array2)
+  array1.concat(array2)
 end
 def using_insert(array,el)
   array.insert(el, 4)
