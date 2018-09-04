2d3bc35456e9f72f28d78515598efabb6e47f249
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-04 20:05:27 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,13 +8,13 @@ def using_pop(array)
   array.pop
 end
 def using_pop_with_args(array)
-  array.push())
+  array.push(2)
 end
-def using_shift(array,el)
-  array.push(el)
+def using_shift(array)
+  array.shift
 end
-def using_shift_with_args(array,el)
-  array.push(el)
+def using_shift_with_args(array)
+  array.shift(2)
 end
 def using_concat(array,el)
   array.push(el)
